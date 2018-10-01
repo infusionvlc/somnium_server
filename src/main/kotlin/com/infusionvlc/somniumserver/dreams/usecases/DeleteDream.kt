4d8ae6bcac75e5351297bca9ab88a2c0dd06a1e3
Cotel <miguelcoletomunoz@hotmail.com>
2018-10-01 18:33:49 +0200

    Refactoring with extension functions for Optional

--- src/main/kotlin/com/infusionvlc/somniumserver/dreams/usecases/DeleteDream.kt
+++ src/main/kotlin/com/infusionvlc/somniumserver/dreams/usecases/DeleteDream.kt
@@ -1,10 +1,10 @@
 package com.infusionvlc.somniumserver.dreams.usecases
 
 import arrow.core.Either
-import arrow.core.Option
 import arrow.core.flatMap
 import arrow.core.left
 import arrow.core.right
+import com.infusionvlc.somniumserver.base.toOption
 import com.infusionvlc.somniumserver.dreams.models.Dream
 import com.infusionvlc.somniumserver.dreams.models.DreamRemovalErrors
 import com.infusionvlc.somniumserver.dreams.persistence.DreamRepository
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component
 @Component
 class DeleteDream(private val dao: DreamRepository) {
   fun execute(id: Long, userId: Long): Either<DreamRemovalErrors, Unit> {
-    val dream = Option.fromNullable(dao.findById(id).orElseGet { null })
+    val dream = dao.findById(id).toOption()
       .map { it.toDomain() }
 
     return dream
